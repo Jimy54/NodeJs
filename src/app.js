f93930bb1a41d6e837861ed486c93fbb3db7757c
DespairNodeChief <programacion@fkinal.edu>
2018-05-04 10:50:27 -0600

    Modulos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict'
+const express = require('express');
+const app = express();
+
+//port
+app.set("port", process.env.PORT || 4120);
+
+app.listen(app.get("port"), () => {
+  console.log("Server running on port 4120");
+});
+
+//headers
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+	next();
+});
+
+//routes
+
+
+//export
+module.exports = app;
